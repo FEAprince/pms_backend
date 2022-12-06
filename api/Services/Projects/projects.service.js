24801c1cf34ee24a8fd167f69f0c2543dadeb5e9
Kedar <hardik.frontendarmy@gmail.com>
2022-12-06 17:00:01 +0530

    []

--- api/Services/Projects/projects.service.js
+++ api/Services/Projects/projects.service.js
@@ -4,14 +4,6 @@ const pagination = require("../../../helper/pagination");
 
 exports.create = async (projects) => {
   try {
-    let date_1 = new Date(projects.projectEndDate);
-    let date_2 = new Date();
-    const days = (date_1, date_2) => {
-      let difference = date_1.getTime() - date_2.getTime();
-      let TotalDays = Math.ceil(difference / (1000 * 3600 * 24));
-      return TotalDays;
-    };
-
     const info = new Projects({
       projectName: projects.projectName,
       projectDescription: projects.projectDescription,
@@ -20,7 +12,10 @@ exports.create = async (projects) => {
       assignUsers: projects.assignUsers,
       projectStatus: projects.projectStatus,
       categoryId: projects.categoryId,
-      daysLeft: days(date_1, date_2),
+      daysLeft: Math.ceil(
+        (new Date(projects.projectEndDate).getTime() - new Date().getTime()) /
+          (1000 * 3600 * 24)
+      ),
     });
 
     const projectData = await info.save();
